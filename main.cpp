e8a4a7e12a4228feb08e63d021de5665bf751908
Misuzi <misuzichan@gmail.com>
2018-05-13 15:48:52 -0500

    Main stuff 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_TESTS 10
+#define MAX_LIST_LENGTH 100
+
 
 using namespace std;
 
@@ -6,8 +11,28 @@ using namespace std;
 
 int main()
 {
-  
+  int *number_list;
+  unsigned long list_length;
+  unsigned int test_num, i;
 
+
+  // Run tests
+  for (test_num = 0; test_num < NUM_TESTS; test_num++)
+  {
+    list_length = (rand() % MAX_LIST_LENGTH) + 1;
+    // Create and populate list
+    number_list = new int[list_length];
+    for (i = 0; i < list_length; i++)
+    {
+
+    }
+
+
+
+
+  }
+
+  // Return results
 
 
   return 0;
